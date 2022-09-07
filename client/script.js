e724beaecdd51e369f37dae9b668ee731d270611
Dave <61114342+DavesCodeMusings@users.noreply.github.com>
2022-09-06 19:09:15 -0500

    Create script.js

---
+++ client/script.js
@@ -0,0 +1,45 @@
+async function getBase() {
+  let url = window.location.href.split('?')[0]  // Remove the ?query_parameter part if present.
+  console.debug('Server URL:', url)
+  let basePromise = await fetch(url + 'base')
+  let baseList = await basePromise.json()
+
+  if (window.location.search == '?sort=name') {
+    baseList.sort((a,b) => a.name.localeCompare(b.name))
+  }
+  else if (window.location.search == '?sort=description') {
+    baseList.sort((a,b) => a.description.localeCompare(b.description))
+  }
+  else {
+    // Default is to sort by spherical distance from the origin.
+    baseList.sort((a,b) => Math.sqrt(a.x**2 + a.y**2 + a.z**2) - Math.sqrt(b.x**2 + b.y**2 + b.z**2))
+  }
+
+  let count = 0
+  let baseHTML = `
+    <table>
+      <thead>
+        <th><a href="/?sort=name" title="Sort by Name">Name</a></th>
+        <th class="numeric"><a href="/?sort=distance" title="Sort by Distance">Coordinates</a></th>
+        <th><a href="/?sort=description" title="Sort by Description">Description</a></th>
+      </thead>
+      <tbody>
+  `
+  baseList.forEach(base => {
+    baseHTML += `
+        <tr>
+          <td>${base.name}</td>
+          <td class="numeric">${base.x}, ${base.y}, ${base.z}</td>
+          <td>${base.description}</td>
+        </tr>
+    `
+    count++
+  })
+  baseHTML += `
+      </tbody>
+    </table>
+    <p>Total bases: ${count}</p>
+  `
+  baseElement = document.getElementById('base')
+  baseElement.innerHTML = baseHTML
+}
